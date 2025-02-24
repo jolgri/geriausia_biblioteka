51e29b1fa6666d01614515fb00a4b8bf40ad813c
Donatas <pasikutis123@gmail.com>
2025-02-24 15:04:59 +0200

    .

--- classes/Reader.js
+++ classes/Reader.js
@@ -1,57 +1,52 @@
-import Book from "./Book.js";
-class Reader {
+import Book from "../classes/Book.js";
 
-    #readerName;
-    #reader_id;
-    #readerCounter;
-    #readerEmail;
-    #readerPincode;
-    static listOfReaaders = [];
-    #borrowed_books =[];
-   
-    constructor(name, email, pincode){
+class Reader {
+  static readerCounter = 1;
+
+  #readerName;
+  #reader_id;
+  #readerEmail; //TODO
+  #readerPincode; //TODO
+  static listOfReaaders = [];
+  #borrowed_books = [];
+
+  constructor(name, email, pincode) {
     this.#readerName = name;
     this.#readerEmail = email;
     this.#readerPincode = pincode;
-    this.#reader_id = 'GBS' +this.#readerCounter;
-    this.#readerCounter += 1; 
-    }
-   
-   
-    getName(){
-        return this.#readerName;
-    }
-   
-    setName(name){
+    this.#reader_id = "GBS " + Reader.readerCounter++;
+  }
+
+  getName() {
+    return this.#readerName;
+  }
+
+  setName(name) {
     this.#readerName = name;
+  }
+
+  getReaderId() {
+    return this.#reader_id;
+  }
+
+  setReaderId(newId) {
+    this.#reader_id = newId;
+  }
+
+  borrowBook(book) {
+    if (
+      book instanceof Book &&
+      book.checkAvailability() === "Knyga yra bibliotekoje"
+    ) {
+      this.#borrowed_books.push(book);
+    } else {
+      throw new Error("Knyga jau yr paskolinta skatytojui");
     }
-    getReaderId() {
-        return this.#reader_id;
-    }
-    setReaderId(newId) {
-        this.#reader_id = newId;
-    }
-    // returnBook(book){
-    //    this.#borrowed_books[].push(book.getInfo());
-    //     } else {
-    //     throw new Error ('Prašome kreiptis į darbuotoją dėl knygos grąžinimo, nes automatizuota sistema');    
-    //     }
-    // }
-    borrowBook(book){
-        if ((book instanceof Book) && (book.checkAvailability() === 'Knyga yra bibliotekoje'))
-        {
-            this.#borrowed_books.push(book);
-        } else {
-        throw new Error ('Knyga jau yr paskolinta skatytojui');    
-        }
-    }
-    
-    getBorrowedBooks(){
-        return this.#borrowed_books;
-    }
-    
+  }
+
+  getBorrowedBooks() {
+    return this.#borrowed_books;
+  }
 }
-
-
 
 export default Reader;
