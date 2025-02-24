a43d1a12eb223e5a30316a9e6e7d0d37709e5c5d
Lexgur <edgaras.malukas@gmail.com>
2025-02-24 14:53:00 +0200

    stilistika

--- users/Reader.js
+++ users/Reader.js
@@ -1,53 +1,52 @@
 import Book from "../classes/Book.js";
 
 class Reader {
-    static readerCounter = 1;
+  static readerCounter = 1;
 
-    #readerName;
-    #reader_id;
-    #readerEmail; //TODO
-    #readerPincode; //TODO
-    static listOfReaaders = [];
-    #borrowed_books =[];
-   
-    constructor(name, email, pincode){
+  #readerName;
+  #reader_id;
+  #readerEmail; //TODO
+  #readerPincode; //TODO
+  static listOfReaaders = [];
+  #borrowed_books = [];
+
+  constructor(name, email, pincode) {
     this.#readerName = name;
     this.#readerEmail = email;
     this.#readerPincode = pincode;
-    this.#reader_id = 'GBS ' + Reader.readerCounter++;
-    }
-   
-    getName(){
-        return this.#readerName;
-    }
+    this.#reader_id = "GBS " + Reader.readerCounter++;
+  }
 
-    setName(name){
+  getName() {
+    return this.#readerName;
+  }
+
+  setName(name) {
     this.#readerName = name;
-    }
+  }
 
-    getReaderId() {
-        return this.#reader_id;
-    }
+  getReaderId() {
+    return this.#reader_id;
+  }
 
-    setReaderId(newId) {
-        this.#reader_id = newId;
-    }
+  setReaderId(newId) {
+    this.#reader_id = newId;
+  }
 
-    borrowBook(book){
-        if ((book instanceof Book) && (book.checkAvailability() === 'Knyga yra bibliotekoje'))
-        {
-            this.#borrowed_books.push(book);
-        } else {
-        throw new Error ('Knyga jau yr paskolinta skatytojui');    
-        }
+  borrowBook(book) {
+    if (
+      book instanceof Book &&
+      book.checkAvailability() === "Knyga yra bibliotekoje"
+    ) {
+      this.#borrowed_books.push(book);
+    } else {
+      throw new Error("Knyga jau yr paskolinta skatytojui");
     }
-    
-    getBorrowedBooks(){
-        return this.#borrowed_books;
-    }
-    
+  }
+
+  getBorrowedBooks() {
+    return this.#borrowed_books;
+  }
 }
-
-
 
 export default Reader;
