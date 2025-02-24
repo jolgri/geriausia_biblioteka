dd76f8274a5970447924c2a43390846ab1b1a520
Donatas <pasikutis123@gmail.com>
2025-02-24 15:21:55 +0200

    all good

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-
 import Category from "./classes/Category.js";
 import Book from "./classes/Book.js";
+import Library from "./classes/Library.js";
+import Rating from "./classes/Rating.js";
+import Reader from "./classes/Reader.js";
 
 
 const newBook = new Book('Ledynmetis', 'Jonas', 'Siaubo', 1234567891014, 17.88, 'Gera knyga', false);
@@ -15,8 +17,6 @@ console.log(newBook2);
 
 const trileriuKategorija = new Category('Trileriai'); // sukuriam nauja kategorija
 const siauboKategorija = new Category('Siaubo'); // sukuriam nauja kategorija
-console.log(trileriuKategorija);  
-console.log(siauboKategorija);
 
 siauboKategorija.addBook(newBook);
 trileriuKategorija.addBook(newBook2); // pridedam knyga prie sukurtos kategorijos
@@ -32,3 +32,19 @@ console.log(trileriuKategorija.getBooksByPriceRange(15, 20)); // isvedam kokios 
 siauboKategorija.setCategoryName('Komedija');
 
 console.log(siauboKategorija);
+
+const library = new Library();
+
+library.addCategory(trileriuKategorija);
+library.addCategory(siauboKategorija);
+
+console.log(library);
+
+const ivertinimas1 = new Rating ('1', 'GBS001', 5, 'Gera skaityti tokią knygą');
+console.log(ivertinimas1.getRating());
+
+const skaitytojas1 = new Reader('Jonas', 'jonas@jonaitis.com', 1234);
+console.log(skaitytojas1);
+
+skaitytojas1.borrowBook();
+console.log(skaitytojas1.getBorrowedBooks);
