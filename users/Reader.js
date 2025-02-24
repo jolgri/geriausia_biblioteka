5777deae354fdfa732de48e26eb766e3014aaf7e
Lexgur <edgaras.malukas@gmail.com>
2025-02-24 14:51:40 +0200

    sutaisytas Reader.js

--- users/Reader.js
+++ users/Reader.js
@@ -1,12 +1,12 @@
 import Book from "../classes/Book.js";
 
 class Reader {
+    static readerCounter = 1;
 
     #readerName;
     #reader_id;
-    #readerCounter;
-    #readerEmail;
-    #readerPincode;
+    #readerEmail; //TODO
+    #readerPincode; //TODO
     static listOfReaaders = [];
     #borrowed_books =[];
    
@@ -14,30 +14,25 @@ class Reader {
     this.#readerName = name;
     this.#readerEmail = email;
     this.#readerPincode = pincode;
-    this.#reader_id = 'GBS' +this.#readerCounter;
-    this.#readerCounter += 1; 
+    this.#reader_id = 'GBS ' + Reader.readerCounter++;
     }
-   
    
     getName(){
         return this.#readerName;
     }
-   
+
     setName(name){
     this.#readerName = name;
     }
+
     getReaderId() {
         return this.#reader_id;
     }
+
     setReaderId(newId) {
         this.#reader_id = newId;
     }
-    // returnBook(book){
-    //    this.#borrowed_books[].push(book.getInfo());
-    //     } else {
-    //     throw new Error ('Prašome kreiptis į darbuotoją dėl knygos grąžinimo, nes automatizuota sistema');    
-    //     }
-    // }
+
     borrowBook(book){
         if ((book instanceof Book) && (book.checkAvailability() === 'Knyga yra bibliotekoje'))
         {
